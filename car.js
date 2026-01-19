9f173d8f40b807f1a89a20f3dc0959fef8781f01
Najoji <josephnaveen550@gmail.com>
2026-01-19 20:24:51 +0530

    added acceleration , speed etc also rotation

--- car.js
+++ car.js
@@ -5,33 +5,62 @@ class Car {
         this.width = width;
         this.height = height;
 
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 4 ;
+        this.friction = 0.08; 
+        this.angle = 0;
+
         this.controls = new Controls();
     }
 
     update() {
         if (this.controls.forward) {
-            this.y -= 2;
+            this.speed += this.acceleration;
         }
         if (this.controls.reverse) {
-            this.y += 2;
+            this.speed -= this.acceleration;
         }
         if (this.controls.left) {
-            this.x -= 2;
+            this.angle += 0.03;
         }
         if (this.controls.right) {
-            this.x += 2;
+            this.angle -= 0.03;
         }
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+
+        this.y -= this.speed;
+
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+             - this.width / 2,
+             - this.height / 2,
             this.width,
             this.height
         );
         ctx.fillStyle = "blue";
         ctx.fill();
+
+        ctx.restore();
     }
 }
